2c4c434061da6536a5a292773f8c8197501fbc4b
usama <usamanadeem1991@gmail.com>
2022-06-07 18:16:37 +0500

    created and instantiated a child class of GeometricObject

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        GeometricObject geometricObject = new GeometricObject("Green", true);
+//        GeometricObject geometricObject = new GeometricObject("Green", true);
+//
+//        System.out.println(geometricObject.toString());
 
-        System.out.println(geometricObject.toString());
+        Circle circle = new Circle(2);
+        System.out.println(circle.toString());
     }
 }
