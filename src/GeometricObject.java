00858d43a7d2818285c5dcb99f723b129b45b915
usama <usamanadeem1991@gmail.com>
2022-06-07 17:48:11 +0500

    created and instantiated a class named GeometricObject

---
+++ src/GeometricObject.java
@@ -0,0 +1,46 @@
+import java.util.Date;
+
+public class GeometricObject {
+    private String color = "white";
+    private boolean filled;
+    private Date dateCreated;
+
+    public GeometricObject() {
+        this.dateCreated = new Date();
+    }
+
+    public GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+        this.dateCreated = new Date();
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String toString() {
+        return "created on " + this.dateCreated +
+                " \ncolor: " + this.color + " and filled " + this.filled;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GeometricObject geometricObject = new GeometricObject("Green", true);
+
+        System.out.println(geometricObject.toString());
     }
 }
